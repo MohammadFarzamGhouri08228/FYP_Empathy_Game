2b2848913d5acac0ff93571cf8ac130343287adb
MohammadFarzamGhouri08228 <mg08228@st.habib.edu.pk>
2025-12-16 20:36:27 +0500

    Bomb Event Dialogue work

---
+++ Assets/Scripts/GameEventType.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+/// <summary>
+/// Enumeration of different game events that can trigger dialogue
+/// </summary>
+public enum GameEventType
+{
+    BombEncountered,
+    FireEncountered,
+    SpikeEncountered,
+    ObstacleEncountered,
+    // Add more event types as needed
+}
